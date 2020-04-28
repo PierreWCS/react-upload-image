454c91f8ba420859cdee9d2ff316521e5f3ab134
PierreWCS <pierrelegrain45@gmail.com>
2020-04-28 18:06:07 +0200

    style and comments

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './UploadFile.css';
+import "./UploadFile.css";
 
 const UploadFile = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -9,27 +9,31 @@ const UploadFile = () => {
   let handleChange;
   handleChange = event => {
     const reader = new FileReader();
+    // Wait for the file to be loaded
     reader.addEventListener("load", function() {
       console.log(this);
       setFilePreview(reader.result);
     });
+    // Then display the preview
     reader.readAsDataURL(event.target.files[0]);
     setSelectedFile(event.target.files[0]);
   };
 
   const sendFile = () => {
-    const data = new FormData();
-    data.append("file", selectedFile);
-    const url = "http://localhost:8000/upload";
-    axios({
-      method: "post",
-      data,
-      url,
-      headers: { "Access-Control-Allow-Origin": "http://localhost:3000" | '*'}
-    })
-      .then(
-        alert('Success')
-      );
+    if (selectedFile) {
+      const data = new FormData();
+      data.append("file", selectedFile);
+      const url = "http://localhost:8000/upload";
+      axios({
+        method: "post",
+        data,
+        url,
+        headers: { "Access-Control-Allow-Origin": "http://localhost:3000" | "*" }
+      }).then(() => {
+        alert("Success");
+      });
+    }
+    else alert("You must select an image")
   };
 
   return (
@@ -41,8 +45,16 @@ const UploadFile = () => {
         name="file"
         onChange={handleChange}
       />
-      <button onClick={sendFile}>Send</button>
-      {filePreview ? <img className="imagePreview" src={filePreview} alt="Preview" /> : null}
+      <button className="sendButton" onClick={sendFile}>
+        Send
+      </button>
+      <div className="previewContainer">
+        {filePreview ? (
+          <img className="imagePreview" src={filePreview} alt="Preview" />
+        ) : (
+          <p>No image selected</p>
+        )}
+      </div>
     </div>
   );
 };
